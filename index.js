3dda53b5959448eb8bacfb084de95fb162c3ec37
msalerno93 <msalerno1993@gmail.com>
2022-01-15 20:45:30 -0500

    work on index.js file

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const endPoint = "http://127.0.0.1:3000/api/v1/lists"
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("loaded");
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(list => {
+        console.log(list);
+    })
 })
