5674d5b901599b3e8d808fabbae22d7911f2716c
msalerno93 <msalerno1993@gmail.com>
2022-01-15 23:06:35 -0500

    update index.js file

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const endPoint = "http://127.0.0.1:3000/api/v1/lists"
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch(endPoint)
-    .then(response => response.json())
-    .then(list => {
-        console.log(list);
-    })
-})
+    getLists()
+});
+
+    function getLists() {
+        fetch(endPoint)
+        .then(res => res.json())
+        .then(lists => {
+            lists.data.forEach(list => {
+                const listMarkup = `
+                    <div data-id=${list.id}>
+                        <h3>${list.attributes.title}</h3>
+                        <p>${list.attributes.category.name}</p>
+                        <button data-id=${list.id}>edit</button>
+                    </div>
+                    <br><br> `;
+
+                    document.querySelector('#list-container').innerHTML += listMarkup
+                })
+            })
+    }
