fb514de5c465906fd017213df0803fc4a5d732d1
RTBlanco <ronnytoribio1@hotmail.com>
2022-01-18 21:14:32 -0500

    removed data from post

--- index.js
+++ index.js
@@ -9,52 +9,53 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
-    function getLists() {
-        fetch(endPoint)
-        .then(res => res.json())
-        .then(lists => {
-            lists.data.forEach(list => {
-                const listMarkup = `
-                    <div data-id=${list.id}>
-                        <h3>${list.attributes.title}</h3>
-                        <p>${list.attributes.category.name}</p>
-                        <button data-id=${list.id}>edit</button>
-                    </div>
-                    <br><br> `;
-
-                    document.querySelector('#list-container').innerHTML += listMarkup
-                })
-            })
-    }
-
-    function createFormHandler(e) {
-        e.preventDefault()
-        const titleInput = document.querySelector('#input-title').value
-        const descriptionInput = document.querySelector('#input-description').value
-        const categoryId = parseInt(document.querySelector('#categories').value)
-        postFetch(titleInput, descriptionInput, categoryId)
-    }
-
-    function postFetch(title, description, category_id) {
-        const bodyData = {title, description, category_id}
-        fetch(endPoint, {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(bodyData)
-        })
-        .then(response => response.json())
-        .then(list => {
-            const listData = list.data
-            console.log(listData)
+function getLists() {
+    fetch(endPoint)
+    .then(res => res.json())
+    .then(lists => {
+        lists.data.forEach(list => {
             const listMarkup = `
-            <div data-id=${list.id}>
-                <h3>${listData.attributes.title}</h3>
-                <p>${listData.attributes.category.name}</p>
-                <button data-id=${listData.id}>edit</button>
-            </div>
-            <br><br> ` ;
-        
-            document.querySelector('#list-container').innerHTML +=
-            listMarkup;
+                <div data-id=${list.id}>
+                    <h3>${list.attributes.title}</h3>
+                    <p>${list.attributes.category.name}</p>
+                    <button data-id=${list.id}>edit</button>
+                </div>
+                <br><br> `;
+
+                document.querySelector('#list-container').innerHTML += listMarkup
+            })
         })
-    }
+}
+
+function createFormHandler(e) {
+    e.preventDefault()
+    const titleInput = document.querySelector('#input-title').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const categoryId = parseInt(document.querySelector('#categories').value)
+    postFetch(titleInput, descriptionInput, categoryId)
+}
+
+function postFetch(title, description, category_id) {
+    const bodyData = {title, description, category_id}
+    fetch(endPoint, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(bodyData)
+    })
+    .then(response => response.json())
+    .then(list => {
+        console.log(list)
+        const listData = list
+        console.log(listData)
+        const listMarkup = `
+        <div data-id=${list.id}>
+            <h3>${listData.title}</h3>
+            <p>${listData.category.name}</p>
+            <button data-id=${listData.id}>edit</button>
+        </div>
+        <br><br> ` ;
+    
+        document.querySelector('#list-container').innerHTML +=
+        listMarkup;
+    })
+}
